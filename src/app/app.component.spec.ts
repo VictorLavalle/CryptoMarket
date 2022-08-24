92bfed9b6ee9b2290ed902da18683b420a59f317
Victor Lavalle <vl@wayakna.com>
2022-08-23 19:36:02 -0500

    🎨 Improvement
    WIP

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'coin-market'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('coin-market');
+    expect(app.titles).toEqual('coin-market');
   });
 
   it('should render title', () => {
